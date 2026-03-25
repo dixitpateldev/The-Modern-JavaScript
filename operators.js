8dec303436557b315a7fa8a7aa95fabf3dc20572
Dixit Patel <nocodedixit@gmail.com>
2026-03-25 17:46:18 +0530

    JavaScript

---
+++ operators.js
@@ -0,0 +1,27 @@
+//Arithmatic Operators
+// let a = 5;
+// let b = 2;
+
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+
+// console.log("a % b =", a % b); //Modulus
+
+// console.log("a ** b =", a ** b); //Exponentiation
+
+//Unary operators
+let a = 5;
+let b = 2;
+
+console.log("a = ", a, "& b =", b);
+
+console.log("++a = ", ++a);
+console.log("a = ", a);
+
+console.log("--b", --b);
+console.log("b = ", b);
+
+//assignment operators
+//= += -= *= %= **=
---
+++ variable and object.js
@@ -0,0 +1,34 @@
+let fullName = "Dixit Patel";
+let age = 33;
+let x = null;
+let y = undefined;
+isFollow = true;
+let z = BigInt (123);
+
+const student = {
+    fullName : "Dixit Patel",
+    age : 33,
+    cgpa : 9.5,
+    isPass : true
+};
+student["age"] = student["age"] + 1;
+
+const product = {
+    productName : "Parker",
+    rating : 4,
+    offer : 5,
+    isDeal : true,
+    price : 270,
+
+};
+
+const profile = {
+    fullName : "dixitpatel",
+    isFollow : true,
+    posts : 195,
+    followers : 569000,
+    following : 4,
+    designation : "Full-stack developer"
+};
+
+console.log(profile);
